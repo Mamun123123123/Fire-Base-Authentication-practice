b6a7a26321e8335c8444c8b4916f869e7d715f9c
Mamun123123123 <almalm90459@gmail.com>
2026-03-28 11:58:14 +0600

    firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCEVzakb1rVyAmBaVcLgWjtolOLJ1kIaWk",
+  authDomain: "fir-learn-723e0.firebaseapp.com",
+  projectId: "fir-learn-723e0",
+  storageBucket: "fir-learn-723e0.firebasestorage.app",
+  messagingSenderId: "189267132831",
+  appId: "1:189267132831:web:3930ab8fc3ea0b97fdfdaa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
