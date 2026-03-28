955ff4aee6edc4ecade2c0d9df17bbcef5be4c8d
Mamun123123123 <almalm90459@gmail.com>
2026-03-28 12:09:12 +0600

    install react router dom and link pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const App = () => {
   return (
-    <div>
-     
+    <div className='p-5 container mx-auto'>
+       <nav>
+        <ul className='flex items-center justify-center space-x-4'>
+          <li><Link to={"/register"} className='px-6 py-2 text-white rounded hover:bg-green-700 bg-green-500'>Register</Link></li>
+          <li><Link to={"/login"} className='px-6 py-2 text-white rounded hover:bg-green-700 bg-green-500'>Login</Link></li>
+
+        </ul>
+       </nav>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter,Route,Routes } from "react-router";
+import Register from './component/Register.jsx';
+import Login from './component/Login.jsx';
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/login" element={<Login/>} />
     </Routes>
   </BrowserRouter>,
 )
